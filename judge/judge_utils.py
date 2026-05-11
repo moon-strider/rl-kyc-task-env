47ff1acc38160434c5319c315578c1bd233279a4
moon-strider <my.jobo@pm.me>
2026-05-11 22:31:41 +0400

    move evaluator into package

--- judge/judge_utils.py
+++ judge/judge_utils.py
@@ -1,10 +1,3 @@
 from __future__ import annotations
 
-import sys
-from pathlib import Path
-
-REPO_ROOT = Path(__file__).resolve().parents[1]
-if str(REPO_ROOT) not in sys.path:
-    sys.path.insert(0, str(REPO_ROOT))
-
-from task.tools.eval_core import *                   
+from rl_kyc_task_env.evaluation import *
